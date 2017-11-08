7fb11f017bc5ae2c5268c43cfe9b6d75d62e10e1
AppleZhang993 <zyp0124@foxmail.com>
2017-11-08 09:54:01 +0800

    基于MNIST训练，用SVHN测试

--- classifier/mnist_cnn.py
+++ classifier/mnist_cnn.py
@@ -24,22 +24,23 @@ epochs = 12
 # input image dimensions
 img_rows, img_cols = 28, 28
 
-# the data, shuffled and split between train and test sets
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-# sio.savemat('mnist_train_28x28.mat', {'X':x_train,'y':y_train})
-# sio.savemat('mnist_test_28x28.mat', {'X':x_test,'y':y_test})
-print(set(y_test))
-
 def read_mat(mat_file):
     data=sio.loadmat(mat_file)
     X = data['X'] #(32,32,3,#)
-    y = data['y'] 
+    y = data['y']
     return X, y
 
-data_home = '../datasets/svhn2mnist/'
-x_train, y_train = read_mat(os.path.join(data_home, 'svhn/svhn_gray_train_28x28.mat'))
-x_test, y_test = read_mat(os.path.join(data_home, 'svhn/svhn_gray_test_28x28.mat'))
-    
+
+# the data, shuffled and split between train and test sets
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+print(x_train.shape,y_train.shape)
+print(set(y_test))
+
+
+#data_home = '../datasets/svhn2mnist/'
+#x_train, y_train = read_mat(os.path.join(data_home, 'svhn/svhn_gray_train_28x28.mat'))
+#x_test, y_test = read_mat(os.path.join(data_home, 'svhn/svhn_gray_test_28x28.mat'))
+
 if K.image_data_format() == 'channels_first':
     x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
     x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
@@ -53,13 +54,13 @@ x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
 x_train /= 255
 x_test /= 255
-print('x_train shape:', x_train.shape)
+print('y_train shape:', y_train.shape,'y_test.shape',y_test.shape)
 print(x_train.shape[0], 'train samples')
 print(x_test.shape[0], 'test samples')
 
 # convert class vectors to binary class matrices
-y_train = keras.utils.to_categorical(y_train, num_classes)
-y_test = keras.utils.to_categorical(y_test, num_classes)
+#y_train = keras.utils.to_categorical(y_train, num_classes)
+#y_test = keras.utils.to_categorical(y_test, num_classes)
 
 model = Sequential()
 model.add(Conv2D(32, kernel_size=(3, 3),
@@ -73,17 +74,32 @@ model.add(Dense(128, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(num_classes, activation='softmax'))
 
-model.compile(loss=keras.losses.categorical_crossentropy,
+model.compile(loss=keras.losses.sparse_categorical_crossentropy,
               optimizer=keras.optimizers.Adadelta(),
               metrics=['accuracy'])
 
-# model.fit(x_train, y_train,
-          # batch_size=batch_size,
-          # epochs=epochs,
-          # verbose=1,
-          # validation_data=(x_test, y_test))
-model.load_weights('./mnist_weights_0.98.h5')
-score = model.evaluate(x_train, y_train, verbose=0)
+model.fit(x_train, y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          verbose=1,
+          validation_data=(x_test, y_test))
+score = model.evaluate(x_test, y_test.reshape(-1,), verbose=0)
+model.save_weights('./mnist_weights.h5')
+
+data_home = '../datasets/svhn2mnist/'
+x_train, y_train = read_mat(os.path.join(data_home, 'svhn/svhn_gray_train_28x28.mat'))
+x_test, y_test = read_mat(os.path.join(data_home, 'svhn/svhn_gray_test_28x28.mat'))
+
+if K.image_data_format() == 'channels_first':
+    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
+    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
+    input_shape = (1, img_rows, img_cols)
+else:
+    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+    input_shape = (img_rows, img_cols, 1)
+
+score = model.evaluate(x_train, y_train.reshape(-1,), verbose=0)
 print('Train loss:', score[0])
 print('Train accuracy:', score[1])
 score = model.evaluate(x_test, y_test, verbose=0)
